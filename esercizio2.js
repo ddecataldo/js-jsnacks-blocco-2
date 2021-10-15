6ca02ad24ab919b319d11a628cb2a53c7c4ea427
ddecataldo <davidedecataldo92@gmail.com>
2021-10-15 17:36:09 +0200

    Fix

--- esercizio2.js
+++ esercizio2.js
@@ -1,28 +1,43 @@
-/*** 
- * TRACCIA
+/*
  * Crea due array che hanno un numero di elementi diversi. 
- * Aggiungi elementi all’array che ha meno elementi fino a quando ne avrà tanti quanti l’altro.  
-
-***/
+ * Aggiungi elementi all'array che ha meno elementi 
+ * fino a quando ne avrà tanti quanti l'altro.
+ */
 
 const array1 = [15, 2, 7];
-const array2 = [5];
+const array2 = [5, 27, 56, 4 ,6, 7, 9];
 
-for(i = 0; i < array1.length; i++ ){
-    // In questo momento salvo nella variabile numeroElementiArray1 tutti gli elementi di array1
-    const numeroElementiArray1 = array1[i];
-    console.log(numeroElementiArray1);
+let arrayPiccolo;
+let arrayGrande;
 
-    for (y = 0; i < array2.length; i++){
-        // In questo momento salvo nella variabile numeroElementiArray2 tutti gli elementi di array2
-        const numeroElementiArray2 = array2[i];
-        console.log(numeroElementiArray2);
-    }
-
+// controllo lunghezze array
+// se array1 e' lungo quanto l'array 2 -> fine
+if( array1.length === array2.length){
+  console.log('I due array sono di uguale lunghezza. FINE')
+  arrayPiccolo = array1;
+  arrayGrande = array2;
+} 
+// altrimenti, se l'array1 e' piu piccolo dell'array2
+else if (array1.length < array2.length){
+  // etichetto l'array1 come il piu piccolo tra i due
+  arrayPiccolo = array1
+  arrayGrande = array2
+  console.log('array1 < array2')
+} 
+// altrimenti ( unico caso restante ) 
+else {
+  // etichetto l'array2 come il piu piccolo tra i due
+  arrayPiccolo = array2
+  arrayGrande = array1
+  console.log('array2 < array1')
 }
 
-// Confronta i due array
+// caloclo da differenza tra i due
+const differenza = arrayGrande.length - arrayPiccolo.length
 
-// Se uno dei due array ha più elementi dell'altro deve pushare gli elementi in più fino a 
-// raggiungere la parità di elementi
+// aggiungo tanti elementi all'array piu piccolo rispetto a quanto e' la differenza tra i due
+for(let i = 0; i < differenza; i++ ){
+  arrayPiccolo.push(Math.random())
+}
 
+console.log(arrayGrande, arrayPiccolo)
